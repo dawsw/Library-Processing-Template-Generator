41cf1a5a44f21272c968cbf36f97d0614090ee41
Dawson Weilage <dawsw124@gmail.com>
2025-02-17 01:13:24 -0600

    Changed PDF download name

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from flask import Flask, url_for, request, render_template, redirect, send_file
-import os
-from io import BytesIO
-from reportlab.lib import colors
-from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.pagesizes import LETTER
-from reportlab.lib.units import inch
-from reportlab.platypus import Table, TableStyle, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from helpers import generatePDF
-from PIL import Image
+from helpers import generatePDF, createLabelImages
 
 app = Flask(__name__)
 
@@ -19,11 +10,17 @@ def index():
         locationList = request.form.getlist("location")
         directionList = request.form.getlist("direction")
         notes = request.form.get("notes")
-        imageList = request.files.getlist("images")
+        imageList = request.files.getlist("imageInput")
 
+        #if user didn't upload images
+        if all(file.filename == ''for file in imageList):
+            imageList = ''
+
+        #create PDF
         pdf = generatePDF(labelList, locationList, directionList, notes, imageList)
 
-        return send_file(pdf, as_attachment=False, download_name="Library_Processing_Template.pdf", mimetype="application/pdf")
+        # Send the PDF file to the user
+        return send_file(pdf, as_attachment=False, download_name="Library_Processing_Template", mimetype="application/pdf")
 
     return render_template('index.html')
 
